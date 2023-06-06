cac7c30457291a5fad2119db9b90361dfbf1b79c
jonathan.villalobos <jonathan.villalobos@CRHER-5B4M893.amer.thermo.com>
2023-06-06 13:22:43 -0600

    Commit Práctica #2 recopilación Semana 1, 2 y 3.

---
+++ src/test/java/com/laliga/LaligaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laliga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaligaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
